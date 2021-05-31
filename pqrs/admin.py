ef68143c6e4d3b30041ab86a8c798b263d120f53
Miguel Cubides <mcubides@gmail.com>
2021-05-31 08:02:59 -0500

    feat type pqr admin form

--- pqrs/admin.py
+++ pqrs/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from pqrs.models import Type, SubType, PQR,PQRSInbound
 
-admin.site.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Fieldset', {'fields': ['name', 'description', 'asociated_icon', 'max_response_days', 'min_response_days']}),
+    ]
+    readonly_fields = ('name',)
+
+admin.site.register(Type, TypeAdmin)
 admin.site.register(SubType)
 admin.site.register(PQR)
 admin.site.register(PQRSInbound)
