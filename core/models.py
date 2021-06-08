f2f08dda8a1c0dd3433be7d93fcf40ee9a2460a1
CharlieBGood <carlosbueno1196@gmail.com>
2021-06-08 13:58:49 -0500

    refactor: Delete remaining polls code in other apps

--- core/models.py
+++ core/models.py
@@ -238,39 +238,3 @@ class Consecutive(models.Model):
     def __str__(self):
         return f"{self.current} {self.date} {self.type}"
     
-
-# Generic answer options model    
-class AnswerOption(models.Model):
-    number = models.PositiveIntegerField()
-    description = models.CharField(blank=False, max_length=50, default='')
-    
-    def __str__(self):
-        return f"{self.number} {self.description}"
-
-# Generic question model, has one set of answer options to extend the answer options model
-class Question(models.Model):
-    description = models.CharField(blank=False, max_length=256, default='')
-    answer_options = models.ManyToManyField(AnswerOption)
-    
-    def __str__(self):
-        return f"{self.description} {self.answer_options}"
-    
-  
-# Generic poll model, has several questions to extend several question models  
-class Poll(models.Model):
-    type_poll = models.PositiveIntegerField()
-    questions = models.ManyToManyField(Question)
-    valid_since = models.DateTimeField(null=True, blank=True)
-    valid_until = models.DateTimeField(null=True, blank=True)
-    
-    def __str__(self):
-        return f"{self.questions} {self.valid_since} {self.valid_until} "
-
-# Specific instance of a poll model, has one poll to extend from the generic poll model
-class PollInstance(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    answers = ArrayField(models.PositiveIntegerField())
-    
-    def __str__(self):
-        return f"{self.poll} {self.answers}"
-    
