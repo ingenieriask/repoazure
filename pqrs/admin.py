3d6126cbaf7604e22a26f07d872849012686b11f
CharlieBGood <carlosbueno1196@gmail.com>
2021-06-22 10:57:00 -0500

    fix: Deactivation of create PQRS in admin module

--- pqrs/admin.py
+++ pqrs/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from pqrs.models import PQRS, Type, SubType, PqrsContent, Topic, InterestGroup
+
 class TypeAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Fieldset', {'fields': ['name', 'description', 'asociated_icon', 'max_response_days', 'min_response_days']}),
     ]
     readonly_fields = ('name',)
 
+class PQRSAadmin(admin.ModelAdmin):
+    
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 admin.site.register(Type, TypeAdmin)
 admin.site.register(SubType)
-admin.site.register(PQRS)
+admin.site.register(PQRS, PQRSAadmin)
 admin.site.register(PqrsContent)
 admin.site.register(Topic)
 admin.site.register(InterestGroup)
