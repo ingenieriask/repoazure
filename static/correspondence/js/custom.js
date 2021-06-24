a82bab2a240ab827a0bc9b6134d1965ff21151c7
NicoRLasso <nicolas.rodriguez@skillnet.com.co>
2021-06-24 09:57:26 -0500

    Feat: add custom to template and add function for search and filter in search_user html

--- static/correspondence/js/custom.js
+++ static/correspondence/js/custom.js
@@ -1,27 +1,35 @@
-$(document).ready(function(){
+function setSearch(numId, name, parent_name) {
+  console.log(numId, name, parent_name)
+  $('#headerForm').html(parent_name + '/' + name)
+  $('#searchpk').val(numId)
+}
+function cleanSearch() {
+  $('#headerForm').html('')
+}
+$(document).ready(function () {
 
-    // $("#id_item" ).autocomplete({
-    //   source: "/correspondence/autocomplete",
-    // });
+  // $("#id_item" ).autocomplete({
+  //   source: "/correspondence/autocomplete",
+  // });
 
-    //$('#id_current_user').selectpicker();
-    //$('#id_person').selectpicker();
-    
-    $("#id_document_file").fileinput({
-        theme: 'fas',
-        allowedFileExtensions: ['pdf','docx','png','jpg','jpeg'],
-        overwriteInitial: true,
-        maxFileSize:20000,
-        maxFilesNum: 1,
-        language: 'es',
-        slugCallback: function (filename) {
-            return filename.replace('(', '_').replace(']', '_');
-        }
-      });
+  //$('#id_current_user').selectpicker();
+  //$('#id_person').selectpicker();
 
-      $('#id_office').on('change',function(evt){
-        $('#id_doctype').html('<option>Example 1</option><option>Example2</option>').selectpicker('refresh');
+  $("#id_document_file").fileinput({
+    theme: 'fas',
+    allowedFileExtensions: ['pdf', 'docx', 'png', 'jpg', 'jpeg'],
+    overwriteInitial: true,
+    maxFileSize: 20000,
+    maxFilesNum: 1,
+    language: 'es',
+    slugCallback: function (filename) {
+      return filename.replace('(', '_').replace(']', '_');
+    }
+  });
 
-      });
+  $('#id_office').on('change', function (evt) {
+    $('#id_doctype').html('<option>Example 1</option><option>Example2</option>').selectpicker('refresh');
+
+  });
 
 });
