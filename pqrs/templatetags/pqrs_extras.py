cdee5bb7d60e78465d7157d35a6a926d63b50e2e
CharlieBGood <carlosbueno1196@gmail.com>
2021-06-24 13:52:48 -0500

    refactor: pqrs_extras custom template tags names without camel case

--- pqrs/templatetags/pqrs_extras.py
+++ pqrs/templatetags/pqrs_extras.py
@@ -4,13 +4,13 @@ from core.services import CalendarService
 register = template.Library()
 
 @register.filter(expects_localtime=True)
-def getMissingDays(request_day, max_response_days):
+def get_missing_days(request_day, max_response_days):
     missingDays = CalendarService.get_remaining_business_days(request_day, max_response_days)
     return missingDays
 
 
 @register.filter(expects_localtime=True)
-def getColorTrafficLight(alerts, request_day):
+def get_color_traffic_light(alerts, request_day):
     alerts = alerts.order_by('response_time')
     days_since_radicate = CalendarService.get_business_days_since(request_day)
     color = "#009900"
