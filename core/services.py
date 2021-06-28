9a27f8856abf2cc13f595c8b20e528d627f209b8
CharlieBGood <carlosbueno1196@gmail.com>
2021-06-28 14:33:40 -0500

    docs: Comments in PdfCreationService

--- core/services.py
+++ core/services.py
@@ -225,18 +225,24 @@ class CalendarService(object):
 
 class PDF(FPDF):
 
+    # Page header
     def custom_header(self, pqrs, initial_x_pos, initial_y_pos, sizing_factor, border):
         
+        # Basic parameters of pqrs info
         params = [
             ('No. Radicado', pqrs.number),
             ('Fecha', pqrs.date_radicated.strftime('%Y-%m-%d %I:%M:%S %p')),
             ('Anexos', str(len(pqrs.files.all())))
         ]
         self.set_font('Arial', '', 6.0*sizing_factor)
+        # Starting position in canvas
         self.set_xy(initial_x_pos, initial_y_pos)
+        # Creation of image box
         self.cell(25*sizing_factor, 20.0*sizing_factor, '', border=border)
+        # Insertion of image
         self.image('static/correspondence/assets/img/faviconcopy.png', x=initial_x_pos+(2.0*sizing_factor), 
                    y=initial_y_pos+(2.0*sizing_factor), w=20*sizing_factor, h=15*sizing_factor)
+        # Creation of basic information table
         self.set_x(initial_x_pos+(25.0*sizing_factor))
         self.cell(50*sizing_factor, 5*sizing_factor, 'NOMBRE DE LA DEPENDENCIA', border=border, align='C')
         self.ln(5*sizing_factor)
@@ -247,6 +253,7 @@ class PDF(FPDF):
             self.ln(5*sizing_factor)
         
         self.set_x(initial_x_pos)
+        # Barcode generation
         self.add_font('Barcode', '', 'static/correspondence/assets/fonts/barcode_font/BarcodeFont.ttf', uni=True)
         self.set_font('Barcode', '', 25*sizing_factor)
         self.cell(75.0*sizing_factor, 15.0*sizing_factor, pqrs.number, border=border, align='C')
@@ -254,16 +261,16 @@ class PDF(FPDF):
 
     # Page footer
     def custom_footer(self):
-        # Position at 1.5 cm from bottom
+        # Position from the bottom of the page
         self.set_y(-51)
-        # Arial italic 8
         self.set_font('Arial', '', 8)
-        # Page number
+        # Dependency info
         self.multi_cell(0, 5, 'NOMBRE DE LA DEPENDENCIA O GRUPO\n')
         self.set_font('Arial', '', 6)
         self.multi_cell(0, 5, 'DIRECCIÓN UBICACIÓN DEPENDENCIA O GRUPO\nNÚMERO DE CONTACTO')
         self.set_font('Arial', '', 5)
         self.multi_cell(0, 5, 'CORREO ELECTRÓNICO O DIRECCIÓN SITIO WEB')
+        # Page numbering
         self.set_font('Arial', 'I', 8)
         self.cell(0, 10, 'Página ' + str(self.page_no()), 0, 0, 'C')
 
@@ -272,26 +279,35 @@ class PdfCreationService(object):
     
     @classmethod
     def create_pqrs_confirmation_label(cls, pqrs):
-                
+        
+        # Create pdf instance        
         pdf = PDF()
+        # Add page to pdf
         pdf.add_page()
         initial_x_pos = 30.0
         initial_y_pos = 50.0
         sizing_factor = 2
         border = 0
+        # Create label base in header
         pdf.custom_header(pqrs, initial_x_pos, initial_y_pos, sizing_factor, border)
-        pdf.output('label.pdf', 'F')
+        # Save pdf file
+        #pdf.output('label.pdf', 'F')
 
 
     @classmethod
-    def create_pqrs_summary(cls, pqrs):        
-        
+    def create_pqrs_summary(cls, pqrs):       
+         
+        # Create pdf instance    
         pdf = PDF()
+        # Add page to pdf
         pdf.add_page()
+        # Define left margin for the document
         pdf.set_left_margin(20.0)
         initial_x_pos = 100.0
         initial_y_pos = 12.0
+        # Create custom doc header
         pdf.custom_header(pqrs, initial_x_pos, initial_y_pos, 1, 1)
+        # Insert information inside doc body
         pdf.set_xy(20.0, initial_y_pos+60.0)
         pdf.set_font('Arial', '', 12)
         pdf.multi_cell(0, 5, 'CIUDAD - '+pqrs.date_radicated.strftime('%Y-%m-%d')+'\nTRATAMIENTO\n')
@@ -315,8 +331,8 @@ class PdfCreationService(object):
         
         for annex in pqrs.files.all():
             pdf.multi_cell(0, 5, '      - ' + annex.name)
-            
-        pdf.set_font('Arial', '', 12)
+        # Create custom doc footer
         pdf.custom_footer()
-        pdf.output('summary.pdf', 'F')
+        # Save pdf file
+        #pdf.output('summary.pdf', 'F')
     
