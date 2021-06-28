965788e15b469b2ce252fb3421ad89308bfeab2d
NicoRLasso <nicolas.rlasso@gmail.com>
2021-06-28 09:04:50 -0500

    Feat: Rename send mail service and add constrains for sending mails

--- core/utils_db.py
+++ core/utils_db.py
@@ -6,23 +6,26 @@ import logging
 import re
 import json
 
-from core.services import MailService
+from core.services import Notifications
 
 logger = logging.getLogger(__name__)
+
 
 def get_system_parameter(format_name):
     print('format_name:', format_name)
     return SystemParameter.objects.get(name=format_name)
 
+
 def get_json_system_parameter(format_name):
     return json.loads(get_system_parameter(format_name).value)
+
 
 def process_email(format_name, recipient_list, data):
     try:
         email_format = get_system_parameter(format_name)
         email_format = json.loads(email_format.value)
 
-        MailService.send_mail(
+        Notifications.send_mail(
             replace_data(email_format['subject'], data),
             replace_data(email_format['body'], data),
             'rino@skillnet.com.co',
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from core.models import NotificationsService
 from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import datetime
@@ -12,56 +13,60 @@ from django.core.mail import BadHeaderError, send_mail
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from core.services import CalendarService, MailService
+from core.services import CalendarService, Notifications
+
 
 def holidays(request):
     '''Return the list of holidays for a given year and country'''
 
     # request should be ajax and method should be GET.
-    if request.is_ajax and request.method == "GET":  
-    
+    if request.is_ajax and request.method == "GET":
+
         year = request.GET.get('year', datetime.now().year)
         country = request.GET.get('country', 'CO')
         holidays = [{
             'date': h.date.strftime(r'%Y-%m-%dT00:00:00'),
             'country': h.country.code,
             'local_name': h.local_name
-            } for h in CalendarService.get_holidays(year, country)]
+        } for h in CalendarService.get_holidays(year, country)]
         return JsonResponse(holidays, safe=False, status=200)
 
     return JsonResponse({}, status=400)
+
 
 def weekends(request):
     '''Return the list of saturdays or sundays of an entire year'''
 
     # request should be ajax and method should be GET.
-    if request.is_ajax and request.method == "GET":  
-    
+    if request.is_ajax and request.method == "GET":
+
         year = request.GET.get('year', datetime.now().year)
         day = request.GET.get('day', 'SAT').upper()
 
         # only saturdays or sundays are valid
-        if day in ('SAT', 'SUN'):   
+        if day in ('SAT', 'SUN'):
             holidays = [{
                 'date': w.strftime(r'%Y-%m-%dT00:00:00'),
-                } for w in CalendarService.get_weekends(year, day)]
+            } for w in CalendarService.get_weekends(year, day)]
             return JsonResponse(holidays, safe=False, status=200)
 
     return JsonResponse({}, status=400)
+
 
 def not_working_days(request):
     '''Return the list of configurated days of an entire year'''
 
     # request should be ajax and method should be GET.
-    if request.is_ajax and request.method == "GET":  
+    if request.is_ajax and request.method == "GET":
         year = request.GET.get('year', datetime.now().year)
         formatted_days = [{
-            'date': d.date.strftime(r'%Y-%m-%dT00:00:00'), 
+            'date': d.date.strftime(r'%Y-%m-%dT00:00:00'),
             'type': d.type.name
-            } for d in CalendarService.get_calendar_days(year) if d.type.name != 'workingday'],
+        } for d in CalendarService.get_calendar_days(year) if d.type.name != 'workingday'],
         return JsonResponse(formatted_days, safe=False, status=200)
 
     return JsonResponse({}, status=400)
+
 
 def password_reset_request(request):
     if request.method == "POST":
@@ -86,10 +91,19 @@ def password_reset_request(request):
                     }
                     email = render_to_string(email_template_name, c)
                     try:
-                        MailService.send_mail(subject, email, to=[user.email])
+                        notifications_validate = NotificationsService.objects.filter(
+                            name="SEND_EMAIL")
+                        notifications = notifications_validate[0].notifications.all(
+                        )
+                        for i in notifications:
+                            # Select a Notifications to send mail
+                            if i.name == "EMAIL_PQR_CREATE":
+                                Notifications.send_mail(
+                                    subject, email, to=[user.email])
                     except BadHeaderError:
                         return HttpResponse('Invalid header found.')
-                    return redirect('password_reset_done') #"/core/password_reset/done/")
+                    # "/core/password_reset/done/")
+                    return redirect('password_reset_done')
     password_reset_form = PasswordResetForm()
     return render(request=request, template_name="registration/password_reset.html",
                   context={"password_reset_form": password_reset_form})
