5f85c0c7dd6ab797186e385f958b5849be0415b7
NicoRLasso <nicolas.rlasso@gmail.com>
2021-07-14 10:31:49 -0500

    Feat: add libary django admin confirm in requirements and in settings

--- rino/settings.py
+++ rino/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = env.json('ALLOWED_HOSTS')
 
 # Application definition
 INSTALLED_APPS = [
+    'admin_confirm',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
