5c38d4014ae299238bfa1de42a6836c0bcc93efa
Jorge_Vanegas <javanegasr@gmail.com>
2021-07-15 20:04:40 -0500

    feat: model for the PQRS workflow definition

--- workflow/admin.py
+++ workflow/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from workflow.forms import SignatureFlowForm
-from workflow.models import SignatureFlow
+from workflow.forms import SignatureFlowForm, RadicateFlowAdminForm
+from workflow.models import SignatureFlow, RadicateFlow
 
 class SignatureFlowAdminForm(admin.ModelAdmin):
     form = SignatureFlowForm
 
+class RadicateFlowAdminForm(admin.ModelAdmin):
+    form = RadicateFlowAdminForm
+
 admin.site.register(SignatureFlow, SignatureFlowAdminForm)
+admin.site.register(RadicateFlow, RadicateFlowAdminForm)
--- workflow/forms.py
+++ workflow/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 import json
-from workflow.models import SignatureFlow
+from workflow.models import SignatureFlow, RadicateFlow
 from workflow.services import SignatureFlowService
 from workflow.widgets import SignatureFlowWidget
 
@@ -26,4 +26,22 @@ class SignatureFlowAdminForm(forms.ModelForm):
 
     class Meta:
         model = SignatureFlow
+        fields = ['name', 'description', 'id']
+
+class RadicateFlowAdminForm(forms.ModelForm):
+
+    id = forms.CharField(max_length=50, required=False, widget=SignatureFlowWidget(), label='Graph')
+
+    def clean(self, *args, **kwargs):
+
+        cleaned_data = super(RadicateFlowAdminForm, self).clean()
+        if self.data['id'] != -1:
+            if self.data['graph'].strip():
+                graph = json.loads(self.data['graph'])
+                sf = SignatureFlowService.from_json(graph, self.cleaned_data['id'])
+
+        return cleaned_data
+
+    class Meta:
+        model = RadicateFlow
         fields = ['name', 'description', 'id']
