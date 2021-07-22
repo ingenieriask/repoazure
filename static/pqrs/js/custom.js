30413eef8bb212fa6865830d0dfa641d027516b8
NicoRLasso <nicolas.rlasso@gmail.com>
2021-07-22 09:23:16 -0500

    Fix: alert table and size

--- static/pqrs/js/custom.js
+++ static/pqrs/js/custom.js
@@ -28,39 +28,36 @@ $('#link_agreement').click(function (e) {
 })
 
 $(document).ready(function () {
-  if ($("#tb_request_sender")) {
-    $("#tb_request_sender").DataTable(
-      {
-        "scrollX": true,
-        language: {
-          "sProcessing": "Procesando...",
-          "sLengthMenu": "Mostrar _MENU_ registros",
-          "sZeroRecords": "No se encontraron resultados",
-          "sEmptyTable": "Ningún dato disponible en esta tabla",
-          "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-          "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-          "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-          "sSearch": "",
-          "sSearchPlaceholder": "Escriba una palabra clave para realizar la búsqueda...",
-          "sInfoThousands": ",",
-          "sLoadingRecords": "Cargando...",
-          "oPaginate": {
-            "sFirst": "Primero",
-            "sLast": "Último",
-            "sNext": "Siguiente",
-            "sPrevious": "Anterior"
-          },
-          "oAria": {
-            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-          },
-          "buttons": {
-            "copy": "Copiar",
-            "colvis": "Visibilidad"
-          }
-        }
-      }
-    );
+  if (document.getElementById("tb_request_creator")) {
+    $("#tb_request_creator").dataTable({
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningún dato disponible en esta tabla",
+        sInfo: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sSearch: "",
+        sSearchPlaceholder: "Escriba una palabra clave para realizar la búsqueda...",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando...",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Último",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending: ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending: ": Activar para ordenar la columna de manera descendente",
+        },
+        buttons: {
+          copy: "Copiar",
+          colvis: "Visibilidad",
+        },
+      },
+    });
   }
 });
 
